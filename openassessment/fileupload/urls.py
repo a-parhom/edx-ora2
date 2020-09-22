ce39d50e9945ccea51825b9763654784451fbf6b
parhom <andriy.parhomenko@gmail.com>
2020-09-22 18:07:14 +0300

    Fix urls.py for Juniper

--- openassessment/fileupload/urls.py
+++ openassessment/fileupload/urls.py
@@ -7,6 +7,6 @@ from openassessment.fileupload import views_django_storage, views_filesystem
 
 urlpatterns = [
     url(r'^django/(?P<key>.+)/$', views_django_storage.django_storage, name='openassessment-django-storage'),
-    url(r'^(?P<key>[^|]+)\|(?P<filename>.+)/$', 'filesystem_storage', name='openassessment-filesystem-storage'),
+    url(r'^(?P<key>[^|]+)\|(?P<filename>.+)/$', views_filesystem.filesystem_storage, name='openassessment-filesystem-storage'),
     url(r'^(?P<key>.+)/$', views_filesystem.filesystem_storage, name='openassessment-filesystem-storage'),
 ]
